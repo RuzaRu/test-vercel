310a027f18cc6e861e94149b821a615dfd5ba153
RuzaRu <ruzaruzanovic@Ruzas-Air>
2023-05-19 17:32:13 +0200

    add initMongo

--- server/api/webhooks/all.get.ts
+++ server/api/webhooks/all.get.ts
@@ -1,6 +1,8 @@
 import { webhooks } from '../../db/models'
+import initMongo from '../../db/mongo'
 
 export default defineEventHandler(async (event) => {
+	await initMongo()
 	const data = await webhooks.find({})
 
 	return {
--- server/api/webhooks/index.post.ts
+++ server/api/webhooks/index.post.ts
@@ -1,6 +1,9 @@
 import { webhooks } from '../../db/models'
+import initMongo from '../../db/mongo'
 
 export default defineEventHandler(async (event) => {
+	await initMongo()
+
 	const { data } = await readBody(event)
 	console.log(data)
 	await webhooks.create(data)
