773306f540b9c640aa974168aa12b7992f03dbd4
RuzaRu <ruzaruzanovic@Ruzas-Air>
2023-05-19 17:52:46 +0200

    get request body

--- server/api/webhooks/index.post.ts
+++ server/api/webhooks/index.post.ts
@@ -4,9 +4,9 @@ import initMongo from '../../db/mongo'
 export default defineEventHandler(async (event) => {
 	await initMongo()
 
-	const { data } = await readBody(event)
+	const data = await readBody(event)
 	console.log(data)
-	await webhooks.create(data)
+	await webhooks.create({ data })
 
 	return {
 		status: 'accepted',
